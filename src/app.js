fb506c5a6f2d949c3f57bd9b12b227f412397094
Phodal HUANG <h@phodal.com>
2019-01-25 17:34:10 +0800

    feat: try to make drag zone

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
-// target elements with the "draggable" class
+function dragMoveListener(event) {
+  var target = event.target,
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
+
+window.dragMoveListener = dragMoveListener;
+
 interact('.draggable')
   .draggable({
-    // enable inertial throwing
     inertia: true,
-    // keep the element within the area of it's parent
     restrict: {
       restriction: "parent",
       endOnly: true,
       elementRect: {top: 0, left: 0, bottom: 1, right: 1}
     },
-    // enable autoScroll
     autoScroll: true,
 
-    // call this function on every dragmove event
     onmove: dragMoveListener,
-    // call this function on every dragend event
     onend: function (event) {
       var textEl = event.target.querySelector('p');
 
@@ -26,25 +35,36 @@ interact('.draggable')
     }
   });
 
-function dragMoveListener(event) {
-  var target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+interact('.dropzone').dropzone({
+  accept: '#yes-drop',
+  overlap: 0.75,
 
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+  ondropactivate: function (event) {
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+      dropzoneElement = event.target;
 
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-}
-
-// this is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener;
-
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+    draggableElement.textContent = 'Dragged in';
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+    event.relatedTarget.textContent = 'Dragged out';
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped';
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
 
 function initEditor(target) {
   var editor = new MediumEditor(target, {
